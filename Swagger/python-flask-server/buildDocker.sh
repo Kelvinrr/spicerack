8057937b0aa4f14ce35348dc3d0dbc81fc4ac825
Tyler Thatcher <tthatcher84@gmail.com>
2018-04-12 12:43:41 -0700

    Updated swagger_server spec

--- Swagger/python-flask-server/buildDocker.sh
+++ Swagger/python-flask-server/buildDocker.sh
@@ -1,5 +1,5 @@
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker build -t manager .
-docker run -it -p 8080:8080 -v /Users/thatcher/Desktop/Classes/Capstone/SpiceData/:/app -v /Users/thatcher/.ssh/:/root/.ssh/ manager
+docker run -it -p 8080:8080 -v /path/to/spice/data:/spicedata/ -v /path/to/swagger_server/:/swaggerapp -v path/to/.ssh/:/root/.ssh/ manager
 docker attach manager
